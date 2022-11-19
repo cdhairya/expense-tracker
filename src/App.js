cb3ab6a693f0f78743a2da41cacf586e3091ff0f
Dhairya Chheda <dhairyakchheda@gmail.com>
2022-11-19 17:47:27 +0530

    Transactions list component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Balance from "./components/Balance";
 import Header from "./components/Header";
 import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 const App = () => {
   return (
     <div>
@@ -9,9 +10,11 @@ const App = () => {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
 };
 
+// https://www.youtube.com/watch?v=XuFDcZABiDQ&list=PLillGF-RfqbY3c2r0htQyVbDJJoBFE6Rb&index=2
 export default App;
